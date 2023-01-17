b88d89d88d7344304556126af16951d48650e201
PranitK <Pranit.Khadilkar@talentica.com>
2023-01-17 10:41:05 +0530

    added places routes with dummy data and error handling

--- posts-backend/src/app.ts
+++ posts-backend/src/app.ts
@@ -1,14 +1,26 @@
-import express from "express"
+import express, {
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from "express"
 import bodyParser from "body-parser"
-import { testRouter } from "./routes/test-route"
-import { run } from "./util/database"
 
-run().catch(console.dir)
+import { placesRoutes } from "./routes/places"
+import { HttpError } from "./models/http-error"
+
+// run().catch(console.dir)
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.use(testRouter)
+app.use("/places", placesRoutes)
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.statusCode || 500).json({
+    message: err.message || "Something went wrong. Please type again.",
+  })
+})
 
 app.listen(3000)
